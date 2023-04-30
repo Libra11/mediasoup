3a58fb65064312f12e46609f241734208eee6021
Libra <libra085925@gmail.com>
2023-04-30 21:44:43 +0800

    https

--- test-client/vite.config.js
+++ test-client/vite.config.js
@@ -1,11 +1,25 @@
+/*
+ * @Author: Libra
+ * @Date: 2023-04-26 18:50:03
+ * @LastEditTime: 2023-04-30 21:43:58
+ * @LastEditors: Libra
+ * @Description:
+ */
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    https: {
+      key: fs.readFileSync('./src/ssl/libra.key'),
+      cert: fs.readFileSync('./src/ssl/libra.pem')
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
